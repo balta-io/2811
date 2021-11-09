c65e7c0c78ecce7f43c1f50c0c3a9d87932c045d
Andre Baltieri <andrebaltieri@hotmail.com>
2021-11-09 09:57:46 -0300

    Finalizado paginacao

--- Modulo 6/Blog/Models/Category.cs
+++ Modulo 6/Blog/Models/Category.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Blog.Models
 {
     public class Category
@@ -5,7 +7,8 @@ namespace Blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
-        
-        public IList<Post> Posts { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<Post> Posts { get; set; }
     }
 }
--- Modulo 6/Blog/Program.cs
+++ Modulo 6/Blog/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using Blog;
 using Blog.Data;
 using Blog.Services;
@@ -54,7 +55,12 @@ void ConfigureMvc(WebApplicationBuilder builder)
     builder
         .Services
         .AddControllers()
-        .ConfigureApiBehaviorOptions(options => { options.SuppressModelStateInvalidFilter = true; });
+        .ConfigureApiBehaviorOptions(options => { options.SuppressModelStateInvalidFilter = true; })
+        .AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        });
 }
 
 void ConfigureServices(WebApplicationBuilder builder)
